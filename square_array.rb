6c20658adac84dbfdeb155cde29a71f1c613d70c
Marcus Jun <mjun@calstate.edu>
2018-04-05 01:57:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+#Complicated version using do end and tracking the array's index number
 def square_array(array)
   # your code here
+  index=0
+  array.each do |number|
+    number**=2
+    array[index]=number
+    index+=1
+  end
 end
+
+#Elegant version, works but unfortunately doesn't pass learn test
+#def square_array(array)
+  #array.collect{|number| number**=2}
+#end
